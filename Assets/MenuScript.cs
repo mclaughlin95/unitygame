b3771575f70634d570131aa72217f3ba0854d604
Corey McLaughlin <cmclaughlin95@hotmail.com>
2024-12-13 21:35:03 -0500

    Modified build settings to build the main menu first as the entry point. Added menu music and change the game music

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
+
+    // Menu Music
+    public AudioSource audioSource;
+    public AudioClip music;
+
     void Start() {
-        
+        audioSource.clip = music;
+        audioSource.loop = true;
+        audioSource.Play();
     }
 
     void Update() {
@@ -12,5 +19,9 @@ public class MenuScript : MonoBehaviour {
 
     public void play() {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void exit() {
+        Application.Quit();
     }
 }
