b71191992ca0b0a2a369b8efaac80a145997245f
Corey McLaughlin <cmclaughlin95@hotmail.com>
2024-12-13 20:27:19 -0500

    Added bird jumping and smacking sound when playing

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -7,24 +7,35 @@ public class BirdScript : MonoBehaviour {
     public LogicScript logic;
     public bool birdIsAlive = true;
 
+    public AudioSource audioSource;
+    public AudioClip jumpSound;
+    public AudioClip smackSound;
+
     void Start() {
+        audioSource.clip = jumpSound;
         logic = GameObject.FindGameObjectWithTag("logic").GetComponent<LogicScript>();
         myRigidbody.constraints = RigidbodyConstraints2D.FreezePositionY;
     }
 
     void Update() {
-        if (logic.isPlaying()) {
+        bool isPlaying = logic.isPlaying();
+        if (isPlaying) {
             myRigidbody.constraints = RigidbodyConstraints2D.None;
             myRigidbody.WakeUp();
         }
-        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive) {
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive && isPlaying) {
             myRigidbody.linearVelocity = Vector2.up * this.flapStrength;
+            audioSource.Play();
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        logic.gameOver();
-        birdIsAlive = false;
+        if (birdIsAlive) {
+            audioSource.clip = smackSound;
+            audioSource.Play();
+            birdIsAlive = false;
+            logic.gameOver();
+        }
     }
 
 }
